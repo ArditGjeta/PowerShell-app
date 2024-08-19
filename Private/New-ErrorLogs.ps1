64e95794be1e41a98b8000f1f89c66ade50b1666
Ardit Gjeta <arditgjeta01@gmail.com>
2024-08-18 21:38:45 -0400

    New-ErrorLogs.ps1

--- Private/New-ErrorLogs.ps1
+++ Private/New-ErrorLogs.ps1
@@ -3,17 +3,20 @@ function New-ErrorLogs {
     $name = @('Application', 'Security', 'System')
     
     foreach ($log in $name) {
-        # Get the logs for the current log name
+        # Get Error and Warning logs for the current log name. If you want to get general logs, delete "-EntryType Error, Warning"
         $moniTor = Get-EventLog -LogName $log -Newest 10 -EntryType Error, Warning -ErrorAction SilentlyContinue
         
         # Check if any logs were found
         if ($null -eq $moniTor -or $moniTor.Count -eq 0) {
             Write-Host "No matching entries found in $log log." -ForegroundColor Green
         } else {
+        
             # Formating to show only Time Source and Messages
             $seperated = $moniTor | Format-Table -Property TimeGenerated, Source, Message
+            
             # Display a message indicating which log is being shown
-            Write-Host "These are $log logs :" -ForegroundColor Cyan
+            Write-Host "These are $log logs:" -ForegroundColor Cyan
+            
             # Output the logs to the console
             Write-Output $seperated
         }
