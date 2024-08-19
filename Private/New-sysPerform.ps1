eb19177b6d03bfe42166ae1466ea174e554938b4
Ardit Gjeta <arditgjeta01@gmail.com>
2024-08-19 19:18:34 -0400

    Update New-sysPerform.ps1

--- Private/New-sysPerform.ps1
+++ Private/New-sysPerform.ps1
@@ -1,11 +1,21 @@
 function New-sysPerform {
-    $i = 0
-while ($i -lt 3) {
-    Get-CimInstance -ClassName Win32_PerfFormattedData_PerfOS_Processor | 
-    Select-Object Name, PercentProcessorTime
-    Start-Sleep -Seconds 5
-    $i++
-}
+# Gets a number for how many sequences you want.
+    param (
+        [string]$Sequences
+    )
+    $count = 0
 
-    
+# Gets CPU information in a human readable format
+    $totalRam = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).Sum
+while($true) {
+    $count++
+    $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $cpuTime = (Get-Counter '\Processor(_Total)\% Processor Time').CounterSamples.CookedValue
+    $availMem = (Get-Counter '\Memory\Available MBytes').CounterSamples.CookedValue
+    $date + ' > CPU: ' + $cpuTime.ToString("#,0.000") + '%, Avail. Mem.: ' + $availMem.ToString("N0") + 'MB (' + (104857600 * $availMem / $totalRam).ToString("#,0.0") + '%)'
+    Start-Sleep -s 2
+    if ($count -eq $Sequences) {
+        break
+        }
+    }
 }
