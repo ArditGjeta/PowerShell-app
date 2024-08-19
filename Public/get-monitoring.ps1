ed526578912402305d949e73a70e5497a61b68ce
Ardit Gjeta <arditgjeta01@gmail.com>
2024-08-18 21:42:02 -0400

    get-monitoring.ps1

--- Public/get-monitoring.ps1
+++ Public/get-monitoring.ps1
@@ -1,9 +1,9 @@
 function get-monitoring {
     
-    Write-Output "Below you will see cpu performance on 3 sequences"
+    Write-Output "Below you will see CPU performance on 3 sequences"
     New-sysPerform
 
-    Write-Output "Next command will create a file on C:\test\logevent.txt with 10 newest logs of Application, Security and System"
+    Write-Output "Next command will show 10 newest logs of Application, Security and System"
     Write-Output "Note that to see security log you need to have administrator permissions"
     New-ErrorLogs
     
