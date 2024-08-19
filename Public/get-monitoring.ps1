79b53c3d1e6cf0e08738a46ebd97428fd3dede7a
Ardit Gjeta <arditgjeta01@gmail.com>
2024-08-19 19:21:33 -0400

    Update get-monitoring.ps1

--- Public/get-monitoring.ps1
+++ Public/get-monitoring.ps1
@@ -1,10 +1,20 @@
 function get-monitoring {
     
-    Write-Output "Below you will see CPU performance on 3 sequences"
-    New-sysPerform
+       while ($true) {
+    Write-Output "Below you will see CPU performance"
+    $sequences = Read-Host -Prompt "Enter the number of sequences you want to see (Only Full Numbers): " 
+    if ($sequences -match '\D') {
+        Write-Output "Please use a character that is a digit, do not enter anything else!"
+        
+    } else {
+        New-sysPerform -Nr $sequences
+        break
+    }
+
+}
 
     Write-Output "Next command will show 10 newest logs of Application, Security and System"
-    Write-Output "Note that to see security log you need to have administrator permissions"
+    Write-Output "Note that to see the security log you need to have administrator permissions"
     New-ErrorLogs
     
 }
