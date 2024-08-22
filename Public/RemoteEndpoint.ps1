0ee2c3b002df72209892bb3726ad027a8fab483b
Ardit Gjeta <arditgjeta01@gmail.com>
2024-08-21 20:11:26 -0400

    Update RemoteEndpoint.ps1

--- Public/RemoteEndpoint.ps1
+++ Public/RemoteEndpoint.ps1
@@ -3,13 +3,13 @@ function RemoteEndpoint {
         [string]$computer,
         [string]$user
     )
-    
-    # Ping connection with remote machine
+    # Below are different test connections to determine if machines can communicate with each other.
+    # Ping connection with the remote machine.
     if ($computer) {
         
         $pingResult = Test-NetConnection -ComputerName $computer
             if ($pingResult.PingSucceeded) {
-            Write-Host "Connection Successfull, machine is on the same network" -ForegroundColor Cyan
+            Write-Host "Connection successful, machine is on the same network" -ForegroundColor Cyan
             Start-Sleep -s 2
             Write-Host "Testing Port Connectivity" -ForegroundColor Cyan
         try {
@@ -17,43 +17,29 @@ function RemoteEndpoint {
 
             Write-Host " PowerShell Remoting is working between you and remote machine" -ForegroundColor Cyan
             Start-Sleep -s 2
-            Write-Host "Please wait for remote connection to be establish"
+            Write-Host "Please wait for a remote connection to be established"
 
             try {
+            # After successfully passing the connection tests above, it's time to get remotely connected.
                 $session = Enter-PSSession -ComputerName $computer -Credential $user
                 Write-Host "PowerShell session Success" -ForegroundColor Green
 
             }
             catch {
-                Write-Host "Remote Session not establish, please try again later" -ForegroundColor Yellow
+                Write-Host "Remote Session not established, please try again later" -ForegroundColor Yellow
             }
             
             
         } catch {
             Write-Host "Remote SConnection needs your attention!
-                    `n PowerShell remote test failed! You need to check/make a rule on firewall for port 5985(HTTP) and 5986(HTTPS)" -ForegroundColor Red
+                    `n PowerShell remote test failed! You need to check/make a rule on the firewall for port 5985(HTTP) and 5986(HTTPS)" -ForegroundColor Red
                     Write-Host "`n Verify WinRM listener configuration on remote machine" -ForegroundColor Red
                     Write-Host "`n Look out for any anti-virus blocking any ports" -ForegroundColor Red
             
         }
-            # $PsResult = Test-WSMan -ComputerName $computer
-                # if ($PsResult.StatusCode -eq 0) {
-                #     Write-Host " PowerShell Remoting is working between you and remote machine" -ForegroundColor Cyan
-                #     Start-Sleep -s 2
-                #     Write-Host "Please wait for remote connection to be establish"
-                #     $session = Enter-PSSession -ComputerName $computer -Credential $user
-                #     if ($session.StatusCode -eq 0) {
-                #         Write-Host "PowerShell session Success"
-                #     } else {
-                #         Write-Host "Remote Session not establish, please try again later" -ForegroundColor Yellow
-                #     }
-                # } else {
-                #     Write-Host "Remote SConnection needs your attention!
-                #     `n PowerShell remote test failed! You need to check/make a rule on firewall for port 5985(HTTP) and 5986(HTTPS)" -ForegroundColor Red
-                #     Write-Host "`n Verify WinRM listener configuration on remote machine" -ForegroundColor Red
-                #     Write-Host "`n Look out for any anti-virus blocking any ports" -ForegroundColor Red
-                # }
+            
             } else {
+            # If the connection fails at pinging the remote machine, you need to check the IP of both machines
             Write-Host "Connection Failed! Please check if the remote machine is on the same network " -ForegroundColor Red
         }
     } 
