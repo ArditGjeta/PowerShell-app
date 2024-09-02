caae44f44e1b0cfd05da4401b3c5b892f14f6baf
Ardit Gjeta <arditgjeta01@gmail.com>
2024-09-01 22:37:03 -0400

    Update MonitoringTool.psm1

--- MonitoringTool.psm1
+++ MonitoringTool.psm1
@@ -1,15 +1,18 @@
 $modulePublicPath = Join-Path $PSScriptRoot "Public"
 $modulePrivatePath = Join-Path $PSScriptRoot "Private"
 
+# Import functions from the Private folder (not exported)
 Get-ChildItem -Path $modulePrivatePath -Filter "*.ps1" | ForEach-Object {
     # Dot source the file to import its functions
     . $_.FullName
 }
 
+# Import functions from the Public folder and export them
 Get-ChildItem -Path $modulePublicPath -Filter "*.ps1" | ForEach-Object {
     # Dot source the file to import its functions
     . $_.FullName
 
-    Export-ModuleMember -Function $_.BaseName
-
-}
+    # The function name inside the .ps1 file should match the BaseName of the file
+    $functionName = $_.BaseName
+    Export-ModuleMember -Function $functionName
+    }
