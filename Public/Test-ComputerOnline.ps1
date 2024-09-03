7a8246f1ad095861525cbf9517615fe73205e91b
Ardit Gjeta <arditgjeta01@gmail.com>
2024-09-03 19:51:12 -0400

    Update Test-ComputerOnline.ps1

--- Public/Test-ComputerOnline.ps1
+++ Public/Test-ComputerOnline.ps1
@@ -1,11 +1,14 @@
 ﻿
 function Test-ComputerOnline {
 try {
+    # Declaring the Servers' name to make an exclusion when getting all computers
     $ServerName = "Dida"
-    $ComputerNames = Get-ADComputer -Filter * | Where-Object { $_.Name -ne $ServerName } | Select-Object -ExpandProperty Name
 
+    $ComputerNames = Get-ADComputer -Filter * | Where-Object { $_.Name -ne $ServerName } | Select-Object -ExpandProperty Name
+    # Jobs array will store all computers' name
     $jobs = @()
-    
+
+    # Below is performed a parallel execution to ping all computers at the same time, this will make the process run faster.
     foreach ($computer in $ComputerNames) {
         $jobs += Start-Job -ScriptBlock {
             param ($name)
@@ -17,14 +20,15 @@ try {
     }
     
     $results = $jobs | Wait-Job | Receive-Job
-    
+    # Get credentials of a user with admin access, you can store credentials on a file but for security reasons, I am not storing those.
     $credentials = Get-Credential
 
+    # Checking if computers are online or no
     foreach ($result in $results) {
         if ($result.Online) {
         $PCName = $($result.ComputerName)
             Write-Output "$PCName is online"
-
+    # The line below will use admin credentials and will show which user is logged on to online computers
             $user = Get-WmiObject -Class Win32_ComputerSystem -ComputerName $PCName -Credential $credentials -Authentication PacketPrivacy -ErrorAction SilentlyContinue | Select-Object -ExpandProperty UserName
             if ($null -eq $user) {
             Write-Output "No user is logged on"
@@ -38,5 +42,6 @@ try {
     
     $jobs | Remove-Job
 
+    # Any other error will be throwing this message
     } catch { Write-Host "Unexpected error, please try again later" -ForegroundColor Red }
 }
