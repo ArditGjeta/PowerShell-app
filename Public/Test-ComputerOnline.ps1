2934d3bb7a9cacdf7ccd186d62fc2aaa7cd5f6eb
Ardit Gjeta <arditgjeta01@gmail.com>
2024-09-02 21:34:58 -0400

    Add files via upload
    Test Computers Online is the new script that I have been working on for the last two days. This script shows you which computers are online and offline. For online computers, it tells you who a login user is.
    More updates in the coming days as I am still thinking on how to make the script more efficient and useful.

---
+++ Public/Test-ComputerOnline.ps1
@@ -0,0 +1,42 @@
+﻿
+function Test-ComputerOnline {
+try {
+    $ServerName = "Dida"
+    $ComputerNames = Get-ADComputer -Filter * | Where-Object { $_.Name -ne $ServerName } | Select-Object -ExpandProperty Name
+
+    $jobs = @()
+    
+    foreach ($computer in $ComputerNames) {
+        $jobs += Start-Job -ScriptBlock {
+            param ($name)
+            [PSCustomObject]@{
+                ComputerName = $name
+                Online = Test-Connection -ComputerName $name -Quiet
+            }
+        } -ArgumentList $computer
+    }
+    
+    $results = $jobs | Wait-Job | Receive-Job
+    
+    $credentials = Get-Credential
+
+    foreach ($result in $results) {
+        if ($result.Online) {
+        $PCName = $($result.ComputerName)
+            Write-Output "$PCName is online"
+
+            $user = Get-WmiObject -Class Win32_ComputerSystem -ComputerName $PCName -Credential $credentials -Authentication PacketPrivacy -ErrorAction SilentlyContinue | Select-Object -ExpandProperty UserName
+            if ($null -eq $user) {
+            Write-Output "No user is logged on"
+            }else{
+            Write-Output " Current logon user: $user"
+            }
+        } else {
+            Write-Output "$($result.ComputerName) is offline"
+        }
+    }
+    
+    $jobs | Remove-Job
+
+    } catch { Write-Host "Unexpected error, please try again later" -ForegroundColor Red }
+}
