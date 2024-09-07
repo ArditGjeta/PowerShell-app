acff08bf45d82788bb5e1cb346de82bbeb24e6b5
Ardit Gjeta <arditgjeta01@gmail.com>
2024-09-06 23:02:43 -0400

    Update UsersLoggedOn.ps1

--- Public/UsersLoggedOn.ps1
+++ Public/UsersLoggedOn.ps1
@@ -1,12 +1,14 @@
-﻿$logs = Get-WinEvent -ComputerName 'Server2' -FilterHashtable @{LogName='Security'; Id='4624'} -Credential (Get-Credential) -MaxEvents 10
+# Getting Security logs with ID=4624 to see the last 10 users that have logged on to the machine
+ $logs = Get-WinEvent -ComputerName 'Server2' -FilterHashtable @{LogName='Security'; Id='4624'} -Credential (Get-Credential) -MaxEvents 10
 
+# Function that will return Domain/Workgroup and user logged on to the machine
 Function Get-userlogged {
     param (
        [Parameter(Mandatory=$true)]
         [string]$Message
        )
 
-
+# Text manipulation to get the account name or domain with the space and the first object after space which is the name or domain 
 $user = ($Message -split 'Account Name:\s+')[1] -split '\s' | Select-Object -First 1
 $domain = ($Message -split 'Account Domain:\s+')[1] -split '\s' | Select-Object -First 1
 
@@ -21,9 +23,13 @@ $domain = ($Message -split 'Account Domain:\s+')[1] -split '\s' | Select-Object 
 
 foreach ($log in $logs) {
 
-#Extract message
+# Assign logs message (as a property) to a variable so we can use it as a parameter to our function
 $message = $log.Message
+
+# For our function to work we need to pull only the message
 $userinfo = Get-userlogged -Message $message
+
+# Converting log time to string so we can display it next to the logged user
 $timelogged = $log.TimeCreated.ToString("dd-mm-yyyy HH:mm:ss")
 Write-Output "$userinfo ->   Logged in: $timelogged"
     
